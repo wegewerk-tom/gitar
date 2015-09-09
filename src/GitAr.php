9c7c8654571497e2954db74bdad8bb09e4e8f290
Tom Stec <tsc@wegewerk.com>
2015-09-09 18:25:13 +0200

    First functional version.

--- src/GitAr.php
+++ src/GitAr.php
@@ -3,8 +3,7 @@
 use Garden\Cli\Cli;
 use Seld\CliPrompt\CliPrompt;
 use PHPGit\Git;
-
-$run = new GitAr($argv);
+use TheSeer\DirectoryScanner\DirectoryScanner;
 
 /**
 * GitAr.
@@ -22,20 +21,25 @@ class GitAr
             ->description('Archive all .git directories.')
             // Define the first command: int.
             ->command('restore')
-            ->description('Restore all .git directories.');
+            ->description('Restore all .git directories.')
             ->command('*')
             ->opt('directory:d', 'Root directory for processing.', false, 'string');
-        try {
-            $this->process($argv);
-        }
-        catch(Exception $e) {
-            print($e->getMessage() . PHP_EOL . PHP_EOL);
-        }
+
+        $cliArgs = $cli->parse($argv, true);
+        $this->process($cliArgs);
     }
 
-    function process($argv)
+    function process($cliArgs)
     {
-        $this->command = $cliArgs->getCommand();
+        $this->command   = $cliArgs->getCommand();
+        $this->directory = $cliArgs->getOpt('directory', getcwd());
+        $this->status_file = $this->directory ."/.GitAr.lockfile";
+
+        if(is_dir($this->directory) === FALSE) {
+            print("That's not a directory, silly!". PHP_EOL);
+            exit(500);
+        }
+
         $method_name = $this->command;
         if(method_exists($this, $method_name)) {
             call_user_func_array(array($this, $method_name), array());
@@ -43,109 +47,73 @@ class GitAr
         }
         else {
             print("Unimplemented command.". PHP_EOL);
-            exit(500);
+            exit(501);
         }
+    }
+
+    function getStatus() {
+        if(file_exists($this->status_file)) {
+            return file_get_contents($this->status_file);
+        }
+        else {
+            return $this->setStatus('restored');
+        }
+    }
+
+    function setStatus($status) {
+        file_put_contents($this->status_file, $status);
+        return $status;
     }
 
     function archive() {
+        if($this->getStatus() === "restored") {
 
+          $scanner = new DirectoryScanner();
+          $scanner->addExclude($this->directory .'/.git/HEAD');
+          $scanner->addInclude('*.git/HEAD');
+          $scanner->setFollowSymlinks(FALSE);
+          $renames = array();
+          foreach($scanner($this->directory) as $info) {
+            $oldname = $info->getPath();
+            $renames[$oldname] = substr($oldname, 0, -4) .'.GitAr';
+          }
+          // Must be unset otherwise errors occur during rename();
+          unset($scanner);
+
+          $this->batchRename($renames);
+          $this->setStatus("restored");
+        }
+        else {
+          print "Cannot archive, because GitAr status is ". $this->getStatus() .".". PHP_EOL;
+        }
     }
 
     function restore() {
-        $config = $this->event->getComposer()->getConfig();
-        $baseDir = dirname($config->get('vendor-dir'));
+        if($this->getStatus() === "archived") {
+          $scanner = new DirectoryScanner();
+          $scanner->addInclude('*.GitAr/HEAD');
+          $scanner->setFollowSymlinks(FALSE);
+          $renames = array();
+          foreach($scanner($this->directory) as $info) {
+              $oldname = $info->getPath();
+              $renames[$oldname] = substr(dirname($info), 0, -6) .'.git';
+          }
+          // Must be unset otherwise errors occur during rename();
+          unset($scanner);
 
-        $origin = $baseDir  .'/src/drupal/';
-        $target = $baseDir  .'/build/int/wegewerk/htdocs';
-
-        $OriginGit = new Git();
-        $OriginGit->setRepository($origin);
-
-        $origin_status = $OriginGit->status();
-
-        if(empty($origin_status['changes'])) {
-            // Ensure up-to-date.
-            $OriginGit->pull();
-            $TargetGit = new Git();
-            $TargetGit->setRepository($target);
-            $TargetGit->pull();
-
-            $exclude = array(
-              'sites/default/settings.php', // Always status symlink
-              'sites/default/files', // Always status symlink
-              '.tmp.',
-              '.git',
-              '.DS_Store',
-              'node_modules', // These files are totally different depending on which machine they're built on.
-              '.gitignore', // Ensure vendor directories are included.
-            );
-
-            $config = array(
-              // Always do a dry run to start with unless automatic is selected.
-              'archive'            => TRUE,
-              'delete_from_target' => TRUE,
-              'exclude'            => $exclude,
-              'verbose'            => TRUE,
-              'option_parameters'  => array('-c'),
-              'show_output'        => TRUE,
-            );
-
-            $rsync = new Rsync($config);
-            $rsync->sync($origin, $target);
-
-            if($this->dry_run) {
-              echo "No updates made." . PHP_EOL;
-              exit(0);
-            }
-
-            if(!$this->automatic) {
-              $accept = "";
-              while(preg_match("/[YynN]/", $accept) !== 1) {
-                echo PHP_EOL;
-                echo 'Accept these changes? [yN]: ';
-                $accept = CliPrompt::prompt();
-              }
-
-              if($accept === "y" || $accept === "Y") {
-                echo 'Syncing deploy.' . PHP_EOL;
-              }
-              else {
-                exit(0);
-              }
-            }
-
-            $config['dry_run'] = FALSE;
-            $rsync = new Rsync($config);
-            $rsync->sync($origin, $target);
-
-
-            if($this->no_commit) {
-              echo PHP_EOL. "Updates made to local clone but no commit + push." . PHP_EOL . PHP_EOL;
-              exit(0);
-            }
-
-            $target_status = $TargetGit->status();
-            if(empty($target_status['changes'])) {
-              print("No updates required at target.". PHP_EOL);
-              exit(0);
-            }
-            else {
-              $config = $OriginGit->config();
-              $origin_url = $config['remote.origin.url'];
-              $origin_log = $OriginGit->log('',NULL, array('limit' => 1));
-              $origin_hash = substr($origin_log[0]['hash'], 0, 8);
-              $TargetGit->add('.');
-              $TargetGit->commit("Automatic werk_deploy from $origin_hash $origin_url.");
-              $TargetGit->push('origin', 'master');
-              print("Automatic werk_deploy from $origin_hash $origin_url.". PHP_EOL);
-              exit(0);
-            }
+          $this->batchRename($renames, "restoring");
+          $this->setStatus("restored");
         }
         else {
-            print("Can't deploy to target: Uncommitted changes in project.". PHP_EOL);
-            exit(500);
+          print "Cannot restore, because GitAr status is ". $this->getStatus() .".". PHP_EOL;
         }
     }
 
-
+    function batchRename($renames, $status = "archiving") {
+        $this->setStatus($status);
+        foreach ($renames as $oldname => $newname) {
+            print(ucfirst($status) ." $oldname". PHP_EOL);
+            rename($oldname, $newname);
+        }
+    }
 }
